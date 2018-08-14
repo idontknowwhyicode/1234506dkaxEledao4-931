344c5fc4c1e376b4c089d86d7a77a0b4dac1af7a
idontknowwhyicode <42370029+idontknowwhyicode@users.noreply.github.com>
2018-08-14 14:23:04 -0400

    Update tclose.js

--- commands/tclose.js
+++ commands/tclose.js
@@ -3,15 +3,35 @@ const Discord = require("discord.js")
 
 
 module.exports.run = async (bot, message, args) => {
-  let channel = message.channel;
-  let cName = channel.name;
-
-    let channelName = message.channel.name
-    if(cName.startsWith("appeal-") || cName.startsWith("apply-") || cName.startsWith("ticket-")) {
-      channel.delete();
-    } else {
-      return ("Are you sure its a ticket channel?")
+    if (isCommand(message, "close")) {
+        if (!message.channel.name.startsWith(`ticket-`)) return message.channel.send(`You can't use the close command outside of a ticket channel.`);
+        // Confirm delete - with timeout (Not command)
+        message.channel.send(`Are you sure? Once confirmed, you cannot reverse this action!\nTo confirm, type \`/confirm\`. This will time out in 10 seconds and be cancelled.`)
+            .then((m) => {
+                message.channel.awaitMessages(response => response.content === '-confirm', {
+                        max: 1,
+                        time: 10000,
+                        errors: ['time'],
+                    })
+                    .then((collected) => {
+                        message.channel.delete();
+                    })
+                    .catch(() => {
+                        m.edit('Ticket close timed out, the ticket was not closed.').then(m2 => {
+                            m2.delete();
+                        }, 3000);
+                    });
+            });
     }
+
+
+function isCommand(message) {
+    return message.content.toLowerCase().startsWith(prefix);
+}
+
+function isCommand(message, cmd) {
+    return message.content.toLowerCase().startsWith(prefix + cmd);
+}
 
 }
 
