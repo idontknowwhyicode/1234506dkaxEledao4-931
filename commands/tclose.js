d3b7d26fa9be9fb5617c9bd3eb3f01da395b2061
idontknowwhyicode <42370029+idontknowwhyicode@users.noreply.github.com>
2018-08-14 14:52:33 -0400

    Update tclose.js

--- commands/tclose.js
+++ commands/tclose.js
@@ -7,7 +7,7 @@ module.exports.run = async (bot, message, args) => {
     if (isCommand(message, "close")) {
         if (!message.channel.name.startsWith(`ticket-`)) return message.channel.send(`You can't use the close command outside of a ticket channel.`);
         // Confirm delete - with timeout (Not command)
-        message.channel.send(`Are you sure? Once confirmed, you cannot reverse this action!\nTo confirm, type \`/confirm\`. This will time out in 10 seconds and be cancelled.`)
+        message.channel.send(`Are you sure? Once confirmed, you cannot reverse this action!\nTo confirm, type \`-confirm\`. This will time out in 10 seconds and be cancelled.`)
             .then((m) => {
                 message.channel.awaitMessages(response => response.content === '-confirm', {
                         max: 1,
