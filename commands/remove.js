5207316bc13bf282417ed588de6484a9490c5119
idontknowwhyicode <42370029+idontknowwhyicode@users.noreply.github.com>
2018-08-14 15:05:31 -0400

    Update remove.js

--- commands/remove.js
+++ commands/remove.js
@@ -3,17 +3,17 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
     let channel = message.channel;
     let cName = channel.name;
-    if(cName.startsWith("appeal-") || cName.startsWith("apply-") || cName.startsWith("report-") || cName.startsWith("help-")) {
+    if(cName.startsWith("appeal-") || cName.startsWith("apply-") || cName.startsWith("report-")) {
 
         let user = message.mentions.users.first();
         if(user) {
 
             let addembed = new Discord.RichEmbed()
-            .setTitle("Speler Verwijderd!")
+            .setTitle("❌ User Removed!")
             .setColor("#ff0000")
-            .setDescription(`Naam: ${user}`)
+            .setDescription(`Player Name: ${user}`)
             .setTimestamp()
-            .setFooter(`© ForestMC`, "https://imgur.com/tfBmDbI.png");
+            .setFooter(`© Limit`, "https://imgur.com/KOA8OVl.png");
             
             message.delete()
             message.channel.send({embed: addembed});
@@ -22,8 +22,8 @@ module.exports.run = async (bot, message, args) => {
             SEND_MESSAGES: false,
             READ_MESSAGES: false
         });
-} else message.channel.send("Je moet een speler taggen!");
-      } else message.reply("Dit werkt alleen in tickets.");
+} else message.channel.send("Tag a user: _example: -remove Jimmyy#7622_!");
+      } else message.reply("You can only remove players in tickets.");
 
 }
 
