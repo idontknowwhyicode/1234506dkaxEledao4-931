dbb8c7fe2cfe448914639cbde9e55f2165ec9d35
idontknowwhyicode <42370029+idontknowwhyicode@users.noreply.github.com>
2018-08-15 03:04:38 -0400

    Create poll.js

---
+++ commands/poll.js
@@ -0,0 +1,27 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+   let polllol = args.join(" ").split(" | ");
+  if(!args[0]) return message.channel.send(`-poll "This is a poll?" "1" "2"`);
+
+    let pollEmbed = new Discord.RichEmbed()
+    .setDescription(`📊` + args[0])
+    .addField(`1️⃣  ${args[1]}`, `2️⃣ ${args[2]}`)
+    .setColor("#ffffff")
+    .setTimestamp()
+    .setFooter(`© ForestMC`, "https://imgur.com/tfBmDbI.png");
+	
+    let poll = message.guild.channels.find("name", "poll");
+
+    poll.sendEmbed(pollEmbed).then(async function (message) {
+		await message.react("1️⃣")
+		await message.react("2️⃣")
+    }).catch(function() {})
+
+};
+
+
+module.exports.help = {
+  name: "poll"
+}
