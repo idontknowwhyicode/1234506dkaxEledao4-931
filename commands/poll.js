7d36837ea3129e15b9527554e438238e6b2eca4e
idontknowwhyicode <42370029+idontknowwhyicode@users.noreply.github.com>
2018-08-15 03:21:36 -0400

    Update poll.js

--- commands/poll.js
+++ commands/poll.js
@@ -7,7 +7,7 @@ module.exports.run = async (bot, message, args) => {
 
     let pollEmbed = new Discord.RichEmbed()
     .setDescription(`📊` + args[0])
-    .addField(`:one:  ` + args[2], `:two: ` + args[3])
+    .addField(`:one:  ` + args[1], `:two: ` + args[2])
     .setColor("#ffffff")
     .setTimestamp()
     .setFooter(`© ForestMC`, "https://imgur.com/tfBmDbI.png");
