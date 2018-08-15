5b1f86a5893dbfc2d45933441dd833fe52c1853b
idontknowwhyicode <42370029+idontknowwhyicode@users.noreply.github.com>
2018-08-15 01:31:34 -0400

    Create apply.js

---
+++ commands/apply.js
@@ -0,0 +1,87 @@
+const Discord = require("discord.js")
+
+
+
+module.exports.run = async (bot, message, args) => {
+    let channel = message.channel;
+    let cName = channel.name;
+    if(!cName.startsWith("commands")) { 
+        message.reply("You can't use -apply here!")
+        .then(msg => {
+            msg.delete(5000)
+          })
+    }
+    if(cName.startsWith("commands")) {
+
+    if (message.guild.channels.exists("name", `apply-${message.author.username.toLowerCase()}`)) return message.channel.send(`You already have a ticket open`);
+
+    message.guild.createChannel(`apply-${message.author.username}`, "text").then(c => {
+        let category = message.guild.channels.find("name", "🎫 Tickets");
+        if(category) {
+            c.setParent(category)
+             .then(updated => console.log(`Set the category of ${channel.name} to ${channel.parent.name}`))
+            .catch(console.error);
+           } else message.reply("Category not able to find.")
+           .then(msg => {
+            msg.delete(5000)
+          })
+        let role = message.guild.roles.find("name", "editrolenamehere");
+        let role2 = message.guild.roles.find("name", "@everyone");
+
+
+        c.overwritePermissions(role, {
+            SEND_MESSAGES: true,
+            READ_MESSAGES: true
+        });
+
+        c.overwritePermissions(role2, {
+            SEND_MESSAGES: false,
+            READ_MESSAGES: false
+        });
+
+        c.overwritePermissions(message.author, {
+            SEND_MESSAGES: true,
+            READ_MESSAGES: true
+        });
+
+        let logs = message.guild.channels.find(`name`, "logs");
+        if(!logs){
+            logs = message.guild.createChannel("logs", "text");
+        }
+
+
+        let logsembed = new Discord.RichEmbed()
+        .setColor("#ffffff")
+        .addField(`Discord Console`,`${message.author} created a apply ticket!\nTicket: ${c}`)
+        .setTimestamp()
+        .setFooter(`© Limit`, "https://imgur.com/KOA8OVl.png");
+
+        let tcreateembed = new Discord.RichEmbed()
+        .setTitle("Limit Ticket")
+        .setColor(`#ffffff`)
+        .addField(`New Ticket`,`${c}`)
+        .addField(`How to go to my apply ticket?`, `Click on ${c}`)
+        .setTimestamp()
+        .setFooter(`© Limit`, "https://imgur.com/KOA8OVl.png")
+      
+        logs.send(logsembed)
+        message.channel.send({embed: tcreateembed});
+
+        const embed = new Discord.RichEmbed()
+        .setColor(`#ffffff`)
+        .addField(`Hey ${message.author.username}!`, `Embed for apply ticket!`)
+        .setTimestamp()
+        .setFooter(`© Limit`, "https://imgur.com/KOA8OVl.png")
+        c.send({ embed: embed });
+
+
+    })
+
+
+
+}
+}
+
+module.exports.help = {
+	name: "apply"
+}
