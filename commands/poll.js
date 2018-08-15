ad243ee2876299ed150c7a180caef1b1cb5237cc
idontknowwhyicode <42370029+idontknowwhyicode@users.noreply.github.com>
2018-08-14 20:33:37 -0400

    Update and rename suggestions.js to poll.js

---
+++ commands/poll.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+   let poll = args.join(" ").split(' " ');
+  if(!args[0]) return message.channel.send(`-poll "This is a poll?" "1" "2"`);
+
+    let pollEmbed = new Discord.RichEmbed()
+    .setDescription(`📊` + args[0])
+    .addField(`1️⃣` + args[1], `lol`)
+    .setColor("#ffffff")
+    .setTimestamp()
+    .setFooter(`© ForestMC`, "https://imgur.com/tfBmDbI.png");
+	
+    let poll = message.guild.channels.find("name", "poll");
+
+    message.delete()
+    poll.sendEmbed(pollEmbed).then(async function (message) {
+		await message.react("1️⃣")
+		await message.react("2️⃣")
+    }).catch(function() {})
+
+};
+
+
+module.exports.help = {
+  name: "poll"
+}
