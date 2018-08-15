104cd6b371f92e4634cb45efcfb9a1d966b02c37
idontknowwhyicode <42370029+idontknowwhyicode@users.noreply.github.com>
2018-08-15 11:52:06 -0400

    Update poll.js

--- commands/poll.js
+++ commands/poll.js
@@ -3,11 +3,11 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
    let polls = args.join(" ").split("|");
-   if(!args[0]) return message.reply(`-poll "This is a poll?" "1" "2"`);
+   if(!args[0]) return message.channel.send(`-poll "This is a poll?" "1" "2"`);
     
 
     let pollEmbed = new Discord.RichEmbed()
-    .addField(" ", `:one: + polls[1]` , `:two:  + polls[2]`)
+    .addField("Limit Poll", `:one: + polls[1]` , `:two:  + polls[2]`)
     .setColor("#ffffff")
     .setTimestamp()
     .setFooter(`© ForestMC`, "https://imgur.com/tfBmDbI.png");
