c5aaa3fecd9bda91ba89581e285045107ba86e25
ign5oc <42465939+ign5oc@users.noreply.github.com>
2018-08-18 12:16:36 +1000

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ fs.readdir("./commands/", (err, files) => {
 process.on("error", console.error)
 bot.on("ready", async () => {
     console.log(`${bot.user.username} is now online!`)
-	bot.user.setActivity("do -help | store.limit.rip", {type: "PLAYING"});
+	bot.user.setActivity("on limit.rip | store.limit.rip 🎉", {type: "PLAYING"});
 	
 })
 bot.on("message", async message => {
