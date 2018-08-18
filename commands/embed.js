c58717f6e3a5efce821c42eff35e5297b07e3c94
ign5oc <42465939+ign5oc@users.noreply.github.com>
2018-08-18 15:31:47 +1000

    Update embed.js

--- commands/embed.js
+++ commands/embed.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
   //!embed {message}
-  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Yeah, that isn't going to happen.");
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Sorry, you don't have permission.");
   let embedmessage = args.join(" ");
   let embedEmbed = new Discord.RichEmbed()
   .setColor("#FFFFFF")
