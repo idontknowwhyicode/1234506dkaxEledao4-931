7de64bc61adcf26cb31a6297992fe17b0a20c518
idontknowwhyicode <42370029+idontknowwhyicode@users.noreply.github.com>
2018-08-19 04:06:50 -0700

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ fs.readdir("./commands/", (err, files) => {
 
 	jsfile.forEach((f, i) => {
 		let props = require(`./commands/${f}`)
-		console.log(`${i + 1}: ${f} loaded!`)
+		console.log(`${i + 1}: ${f} loaddded!`)
 		bot.commands.set(props.help.name,props)
 	})
 
