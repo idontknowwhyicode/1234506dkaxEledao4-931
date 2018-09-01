b5db916c7ee7aaa0bccf95ef7fd4a9dcc03bb43a
ign5oc <42465939+ign5oc@users.noreply.github.com>
2018-09-01 18:41:52 +1000

    Update lockdown.js

--- commands/lockdown.js
+++ commands/lockdown.js
@@ -5,9 +5,9 @@ exports.run = (client, message, args) => {
     let validUnlocks = ['release', 'unlock'];
     let modRole = message.guild.roles.find('name', '» Manager');
     if (!message.member.roles.has(modRole.id)) {
-        return message.reply('you have insufficient permissions to use this command.').catch(console.error);
+        return message.reply('You have insufficient permissions to use this command.').catch(console.error);
     }
-    if (!time) return message.reply('you must set a duration for the lockdown in either hours, minutes or seconds.');
+    if (!time) return message.reply('You must set a duration for the lockdown in either hours, minutes or seconds.');
 
     if (validUnlocks.includes(time)) {
         message.channel.overwritePermissions(message.guild.id, {
@@ -23,7 +23,7 @@ exports.run = (client, message, args) => {
         message.channel.overwritePermissions(message.guild.id, {
             SEND_MESSAGES: false
         }).then(() => {
-            message.channel.send(`**Channel locked** for ${ms(ms(time), { long:true })}.`).then(() => {
+            message.channel.send(`-embed **Channel locked** for ${ms(ms(time), { long:true })}.`).then(() => {
 
                 client.lockit[message.channel.id] = setTimeout(() => {
                     message.channel.overwritePermissions(message.guild.id, {
