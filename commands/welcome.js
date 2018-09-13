050d8ba18f43791dc58b2a546e309dcdd1ea8a63
ign5oc <42465939+ign5oc@users.noreply.github.com>
2018-09-13 16:30:18 +1000

    Delete welcome.js

--- commands/welcome.js
+++
@@ -1,4 +0,0 @@
-client.on("guildMemberAdd", (member) => {
-  console.log(`New User "${member.user.username}" has joined "${member.guild.name}"` );
-  member.guild.channels.get("welcome").send(`"${member.user.username}" has joined this server`);
-});
